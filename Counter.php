8be3d8e5358bb1bda5748b433c9b829d6b3a0109
n-gram-hub <54742855+n-gram-hub@users.noreply.github.com>
2019-12-16 11:33:19 +0100

    Update Counter.php

--- Counter.php
+++ Counter.php
@@ -1,8 +1,17 @@
 <?php
 
-function isAssociativeArray($a)
+//ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
+//error_reporting(-1);
+
+trait Utils
 {
-    return is_array($a) && array_diff_key($a, array_keys(array_keys($a)));
+
+    public static function isAssociativeArray($a)
+    {
+        return is_array($a) && array_diff_key($a, array_keys(array_keys($a)));
+    }
+    
 }
 
 /**
@@ -12,7 +21,7 @@ function isAssociativeArray($a)
  * Example usages below, not much tested, use with care
  * In some cases behavior or API slightly differ from the original
  * 
- * Useful for basic frequency dictionary usage
+ * Useful as a starting point for TF-IDF
  * 
  * @author https://github.com/n-gram-hub
  * @version $Revision: 0.1.0 $
@@ -21,7 +30,7 @@ function isAssociativeArray($a)
  */
 class Counter implements ArrayAccess
 {
-
+    use Utils;
     private $counter = [];
     
     /*
@@ -29,7 +38,7 @@ class Counter implements ArrayAccess
     * @param string, array $mixed
     *
     * @throws TypeError if $mixed is not string or array. Please, God of PHP, invent constructor overloading
-    * @throws UnexpectedValueException if array doens't contain valid elements
+    * @throws UnexpectedValueException if array doesn't contain valid elements
     * 
     */
     public function __construct($mixed = [])
@@ -46,7 +55,7 @@ class Counter implements ArrayAccess
         if ($argsLength > 0) {
 
             // if user passed associative array
-            if (isAssociativeArray($mixed)) {
+            if (Utils::isAssociativeArray($mixed)) {
 
                 // if keys are strings or ints && values are ints
                 if ($this->keysAreValid(array_keys($mixed)) && $this->valuesAreValid(array_values($mixed))) {
@@ -78,7 +87,7 @@ class Counter implements ArrayAccess
                     throw new UnexpectedValueException("Each key must be string or integer, each value must be integer");
                 }
 
-            // if is plain array
+            // if $mixed is plain array
             } elseif (is_array($mixed)) {
 
                 // if elements are valid
@@ -89,13 +98,13 @@ class Counter implements ArrayAccess
                 } else {
                     throw new UnexpectedValueException("Each element must be string or integer");
                 }
-            // if is string
+            // if $mixed is string
             } elseif (is_string($mixed)) {
                 
                 // split
                 $this->counter = array_count_values(str_split($mixed));
             }
-            // is completely other type
+            // if $mixed is completely another type
             else {
                 throw new TypeError("You can pass strings, arrays and associative arrays");
             }
@@ -109,35 +118,28 @@ class Counter implements ArrayAccess
     }    
 
 
-    // array access stuff
+    // ArrayAccess stuff
     public function offsetSet($offset, $value)
     {
-        
         if ((is_string($offset)||is_numeric($offset)) && is_int($value)) {
-
             $this->counter[$offset] = $value;
-        
         } 
     }
-
 
     public function offsetExists($offset)
     {
         return isset($this->counter[$offset]);
     }
 
-
     public function offsetUnset($offset)
     {
         unset($this->counter[$offset]);
     }
 
-
     public function offsetGet($offset)
     {
         return isset($this->counter[$offset]) ? $this->counter[$offset] : 0;
     }
-
 
     public function printCounter()
     {
@@ -178,7 +180,7 @@ class Counter implements ArrayAccess
 
     private function arrayIsValid($array)
     {
-        return array_product(array_map(array($this, 'validElement_callback'), $array)) == 1;
+        return $this->keysAreValid($array);
     }
     
 
@@ -207,11 +209,11 @@ class Counter implements ArrayAccess
     // $m can be string, array of strings/ints, associative arrays of counts and Counters
     private function validateMixedType($m){
 
-        if (is_string($m) || (is_array($m) && !isAssociativeArray($m))) {
+        if (is_string($m) || (is_array($m) && !Utils::isAssociativeArray($m))) {
 
             $m = array_count_values(is_string($m) ? str_split($m) : $m);
 
-        } elseif (isAssociativeArray($m) || $m instanceof Counter) {
+        } elseif (Utils::isAssociativeArray($m) || $m instanceof Counter) {
             
             $m = $m instanceof Counter ? $m->getCounter() : $m;
 
@@ -320,179 +322,3 @@ class Counter implements ArrayAccess
     }
 
 }
-
-
-// usage examples
-// example 1, counter with arrayaccess
-
-try{
-    $c = new Counter();
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-
-$tokens = ['red', 'blue', 'red', 'green', 'blue', 'blue'];
-$tokensCount = count($tokens);
-for($i=0; $i<$tokensCount; $i++){
-    $c[$tokens[$i]] += 1;
-}
-
-$c->printCounter(); // Array ( [red] => 2 [blue] => 3 [green] => 1 )
-
-
-
-// example 2, counter initialized by string (converts the string to an array of character counts)
-
-try{
-    $c = new Counter("gallahad");
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-$c->printCounter(); // Array ( [g] => 1 [a] => 3 [l] => 2 [h] => 1 [d] => 1 )
-
-
-
-// example 3, counter initialized by associative array
-
-try{
-    $c = new Counter(['red'=>4, 'blue'=>2]);
-    //$c = new Counter(array('red'=>4, 'blue'=>array())); // UnexpectedValueException
-    //$c = new Counter(true); // TypeError
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-$c->printCounter(); // Array ( [red] => 4 [blue] => 2 )
-
-
-
-// example 4, count of a missing element is 0, setting a count to zero does not remove an element from a counter
-
-try{
-    $c = new Counter(['eggs', 'ham']);
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-print($c['bacon']); // 0
-//$c['ham'] = 0;  
-$c->printCounter(); // ( [eggs] => 1 [ham] => 0 )
-// use unset to remove element
-// unset($c['ham']);
-$c->printCounter();
-
-
-
-// example 5, listElements (same as Python Counter list(elements()))
-
-try{
-    $c = new Counter(['a'=>4, 'b'=>2, 'c'=>0, 'd'=>-2]);
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-print_r($c->listElements()); // Array ( [0] => a [1] => a [2] => a [3] => a [4] => b [5] => b )
-
-
-
-// example 6, mostCommon
-
-try{
-    $c = new Counter('abracadabra');
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-print_r($c->mostCommon(3));   // Array ( [a] => 5 [b] => 2 [r] => 2 )
-
-
-
-// example 7, subtract (for some strange reason, you can have negative counts)
-
-try{
-    $c = new Counter(['a'=>4,'b'=>2,'c'=>0,'d'=>-2]);
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-$d = ['a'=>1,'b'=>2,'c'=>3,'d'=>4];   // Array ( [a] => 3 [b] => 0 [c] => -3 [d] => -6 )
-//$d = ['a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>10];   // Array ( [a] => 3 [b] => 0 [c] => -3 [d] => -6 [e] => -10)
-//$d = ['d','a','b'];   // Array ( [a] => 3 [b] => 1 [c] => 0 [d] => -3 )
-//$d = 'd';   // Array ( [a] => 4 [b] => 2 [c] => 0 [d] => -3 )
-//$d = new Counter(['a'=>1,'b'=>1,'c'=>1,'d'=>1]);   // Array ( [a] => 3 [b] => 1 [c] => -1 [d] => -3 )
-//$d = [];   // Array ( [a] => 4 [b] => 2 [c] => 0 [d] => -2 ) 
-$c->subtract($d);
-$c->printCounter();
-
-
-
-//example 8, update
-
-try{
-    $c = new Counter(['a'=>4,'b'=>2,'c'=>0,'d'=>-2]);
-}
-catch (TypeError $e) {
-    echo "TypeError: " . $e->getMessage();
-}  
-catch (UnexpectedValueException $e) {
-    echo "UnexpectedValueException: " . $e->getMessage();
-}
-catch (Exception $e){
-    echo $e->getMessage();
-}
-
-$d = ['a'=>1,'b'=>1,'c'=>1,'d'=>1];   // Array ( [a] => 5 [b] => 3 [c] => 1 [d] => -1 ) 
-//$d = ['a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>10];   // Array ( [a] => 5 [b] => 4 [c] => 3 [d] => 2 [e] => 10 ) 
-//$d = ['d','a','b'];   // Array ( [a] => 5 [b] => 3 [c] => 0 [d] => -1 ) 
-//$d = 'd';   // Array ( [a] => 4 [b] => 2 [c] => 0 [d] => -1 )
-//$d = new Counter(['a'=>1,'b'=>1,'c'=>1,'d'=>1]);   // Array ( [a] => 5 [b] => 3 [c] => 1 [d] => -1 )
-//$d = [];   // Array ( [a] => 4 [b] => 2 [c] => 0 [d] => -2 ) 
-$c->update($d);
-$c->printCounter();
